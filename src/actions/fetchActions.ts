7c3bb1a3562ae79bb3f280698ea27725e2949b47
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2025-03-30 13:57:22 +0530

    `fetch user coins in fetch action`

---
+++ src/actions/fetchActions.ts
@@ -0,0 +1,20 @@
+import prisma from "@/lib/prisma"
+import {unstable_cache} from "next/cache"
+
+
+export const getUserCoins = unstable_cache(
+
+    async (user_id: number | string)=>{
+        return await prisma.user.findUnique({
+            select:{
+                coins:true
+            },
+            where:{
+                id:Number(user_id)
+            }
+        })
+    },
+    ["userCoins"],
+    {revalidate:60*60 , tags:["userCoins"]}
+)
+
